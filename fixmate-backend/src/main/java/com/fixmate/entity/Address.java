26a0c13852c420b33d012578f2c4cf1952b381a2
Nithin-Marla <marlanithin288@gmail.com>
2026-08-25 03:45:33 +0530

    update trash button

--- fixmate-backend/src/main/java/com/fixmate/entity/Address.java
+++ fixmate-backend/src/main/java/com/fixmate/entity/Address.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 
 @Data
 @Builder
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "addresses")
+@SQLDelete(sql = "UPDATE addresses SET is_deleted = true WHERE id = ?")
 public class Address {
 
     @Id
